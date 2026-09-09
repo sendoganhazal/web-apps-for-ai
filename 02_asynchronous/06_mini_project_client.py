c6ef797e74f50ca9ed8439343f416a1d403ba19e
Hazal Sendogan <sendoganhazal@gmail.com>
2026-09-09 15:05:21 +0300

    3.7. asenkron programlama mini proje part 2

--- 02_asynchronous/06_mini_project_client.py
+++ 02_asynchronous/06_mini_project_client.py
@@ -11,4 +11,29 @@ import requests
 
 BASE_URL = "http://127.0.0.1:8000"
 
-# 1. Kullanıcı Mesajı
+# # 1. Kullanıcı Mesajı
+# kullanici_mesaji = input("Mesajınızı Yazın: ")
+
+# # veri yapısı
+# gonderilecek_mesaj = {
+#     "mesaj": kullanici_mesaji
+# }
+
+# response = requests.post(f"{BASE_URL}/chat", json=gonderilecek_mesaj)
+
+# print(response.json())
+
+while True:
+    # 1. Kullanıcı Mesajı
+    kullanici_mesaji = input("Mesajınızı Yazın: ")
+    if kullanici_mesaji.lower() == "q":
+        break
+
+    # veri yapısı
+    gonderilecek_mesaj = {
+        "mesaj": kullanici_mesaji
+    }
+
+    response = requests.post(f"{BASE_URL}/chat", json=gonderilecek_mesaj)
+
+    print(response.json())
