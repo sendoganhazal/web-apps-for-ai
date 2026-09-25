3b1de92e761b8f36244095ec55c308b2298deac9
Hazal Sendogan <sendoganhazal@gmail.com>
2026-09-25 13:16:16 +0300

    endpoint yazıldı

--- 07_deploy/01_backend/main.py
+++ 07_deploy/01_backend/main.py
@@ -1,2 +1,51 @@
 """FastAPI backend uygulaması ve öğrenme yol haritası uç noktaları."""
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+ROADMAPS = {
+    "yapay_zeka": [
+        "Python Temelleri",
+        "Veri Analizi",
+        "Makine Öğrenmesi",
+        "Model Değerlendirme",
+        "Mini Projeler"
+    ],
+    "derin_ogrenme": [
+        "Python ve NumPy",
+        "Yapay Sinir Ağları",
+        "CNN",
+        "RNN ve LSTM",
+        "Derin Öğrenme Projeleri"
+    ],
+    "nlp": [
+        "Metin Ön İşleme",
+        "Word Embedding",
+        "RNN ve LSTM ile NLP",
+        "Transformer",
+        "RAG ve Chatbot Projeleri"
+    ]
+}
+
+class RoadmapRequest(BaseModel):
+    alan: str
+
+@app.get("/")
+def home():
+    return {"message": "Backend çalışıyor"}
+
+@app.post("/roadmap")
+def get_roadmap(data: RoadmapRequest):
+
+    secim = data.alan.lower()
+
+    if secim not in ROADMAPS:
+        return {
+            "error": "geçersiz seçim"
+        }
+    return{
+        "alan": secim,
+        "adimlar": ROADMAPS[secim]
+    }
